c3afa06e4ec4f3b614c00cf2c1b031fb6f4bf994
kingbary <kingsleyakwa@gmail.com>
2026-03-24 08:38:38 +0100

    method overriding lesson

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,25 @@ void main() {
     // System.out.print("My name is " + name + ", and I am " + number + " years
     // old");
 
-    Car car1 = new Car("Mercedez Benz", "G-Wagon G63", 234478009.00);
-    Car car2 = new Car("Toyota", "Camry 2025 LE", 45045000.59);
-    car1.welcomeMessage();
+    // Car car1 = new Car("Mercedez Benz", "G-Wagon G63", 234478009.00);
+    // Car car2 = new Car("Toyota", "Camry 2025 LE", 45045000.59);
+    // car1.welcomeMessage();
 
-    System.out.println(car1.make);
-    System.out.println(car1.model);
-    System.out.println(car1.price);
-    System.out.println(car1.isBrandNew);
+    // System.out.println(car1.make);
+    // System.out.println(car1.model);
+    // System.out.println(car1.price);
+    // System.out.println(car1.isBrandNew);
 
-    System.out.println(car2.make);
-    System.out.println(car2.model);
-    System.out.println(car2.price);
-    System.out.println(car2.isBrandNew);
+    // System.out.println(car2.make);
+    // System.out.println(car2.model);
+    // System.out.println(car2.price);
+    // System.out.println(car2.isBrandNew);
+
+    Cat cat = new Cat();
+    Dog dog = new Dog();
+    Fish fish = new Fish();
+
+    cat.move();
+    dog.move();
+    fish.move();
 }
